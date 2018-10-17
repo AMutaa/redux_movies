fbcd5b9a8ff1a9646e779a28b3a83ff4ac83b129
AMutaa <piusandru@gmail.com>
2018-10-17 14:30:44 -0400

    -Provider-

--- src/App.js
+++ src/App.js
@@ -6,28 +6,34 @@ import {
   Switch,
   Link
 } from 'react-router-dom'
-
+import { Provider } from 'react-redux';
+import { createStore } from 'redux'
 import './App.css';
 import MoviesList from './MoviesList';
 import MovieDetail from './MovieDetail';
 
+const hello = () => ('hello')
+const store = createStore(hello)
+
 const App = () => (
-  <Router>
-    <div className="App">
-      <header className="App-header">
-        <Link to="/" className="logo_link">
-          <div className="logo">
-            <h1>NET</h1>
-            <h1>MOVIES</h1>
-          </div>
-        </Link>
-      </header>
-      <Switch>
-        <Route exact path="/" component={MoviesList} />
-        <Route path="/:id" component={MovieDetail} />
-      </Switch>
-    </div>
-  </Router>
+  <Provider store={store}>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <Link to="/" className="logo_link">
+            <div className="logo">
+              <h1>NET</h1>
+              <h1>MOVIES</h1>
+            </div>
+          </Link>
+        </header>
+        <Switch>
+          <Route exact path="/" component={MoviesList} />
+          <Route path="/:id" component={MovieDetail} />
+        </Switch>
+      </div>
+    </Router>
+  </Provider>
 )
 
 export default App;
