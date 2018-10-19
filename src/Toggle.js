6cd4c0dad823f8b6d1eb7c20f33ee702adb10bab
AMutaa <piusandru@gmail.com>
2018-10-19 11:50:18 -0400

    action_creators

--- src/Toggle.js
+++ src/Toggle.js
@@ -1,16 +1,13 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { toggleMessage } from './actions'
 
 
 const Toggle = ({ messageVisibility, dispatch }) => {
   return (
     <div>
       {messageVisibility && <p>You'll be seeing this if redux action is toggled</p>}
-      <button onClick={() =>
-        dispatch({
-          type: 'TOGGLE_MESSAGE'
-        })
-      }>Toggle Me</button>
+      <button onClick={() => dispatch(toggleMessage())}>Toggle Me</button>
     </div>
   )
 }
@@ -18,7 +15,6 @@ const Toggle = ({ messageVisibility, dispatch }) => {
 const mapStateToProps = (state) => ({
   messageVisibility: state.message.messageVisibility,
 });
-
 
 export default connect(mapStateToProps)(Toggle);
 
