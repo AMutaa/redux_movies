56e1bc49c8c01373634290c620dd8beab637b1df
AMutaa <piusandru@gmail.com>
2019-07-24 22:29:32 -0400

    update

--- src/Components/movies/MoviesList.js
+++ src/Components/movies/MoviesList.js
@@ -1,37 +1,48 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
-import Movie from './Movie';
-import { getMovies } from './actions'
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import Movie from "./Movie";
+import { getMovies } from "./actions";
 
 class MoviesList extends Component {
   componentDidMount() {
     const { getMovies, isLoaded, moviesLoadedAt } = this.props;
-    const oneHour = 60 * 60 * 1000
-    if (!isLoaded || ((new Date()) - new Date(moviesLoadedAt)) > oneHour) {
-      getMovies();
-    }
+    getMovies();
+    console.log(this.props.movies);
+    // const oneHour = 60 * 60 * 1000
+    // if (!isLoaded || ((new Date()) - new Date(moviesLoadedAt)) > oneHour) {
+    //   getMovies();
+    // }
     // this.props.getMovies();
   }
   render() {
     const { movies, isLoaded } = this.props;
-    if (!isLoaded) return <h1>Loading</h1>
+    if (!isLoaded) return <h1>Loading</h1>;
     return (
       <div className="movie_grid">
-        {movies.map(movie => <Movie key={movie.id} movie={movie} />)}
+        {movies.map(movie => (
+          <Movie key={movie.id} movie={movie} />
+        ))}
       </div>
     );
   }
 }
 
-const mapStateToProps = (state) => ({
+const mapStateToProps = state => ({
   movies: state.movies.movies,
   isLoaded: state.movies.moviesLoaded,
-  moviesLoadedAt: state.movies.moviesLoadedAt,
+  moviesLoadedAt: state.movies.moviesLoadedAt
 });
 
-const mapDispatchToProps = (dispatch) => bindActionCreators({
-  getMovies,
-}, dispatch);
+const mapDispatchToProps = dispatch =>
+  bindActionCreators(
+    {
+      getMovies
+    },
+    dispatch
+  );
 
-export default connect(mapStateToProps, mapDispatchToProps)(MoviesList);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(MoviesList);
